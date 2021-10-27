8678e1407991f341bedc11dd48eb58150a7738cb
liuxiaoxue <liuxiaoxue@starlinke.com>
2021-10-27 16:16:56 +0800

    变更项目路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -192,6 +192,7 @@ module.exports = {
         title: "项目",
         collapsable: true,
         children: [
+          { title: "setting+vscode", path: "/sideBar/project/problem" },
           { title: "setting+vscode", path: "/sideBar/project/setting" },
           { title: "工具", path: "/sideBar/project/until" },
           { title: "插件", path: "/sideBar/project/public" },
