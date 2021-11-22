32eee9fdc9d8db17aee418f21dfd677ecf78e52d
“liuxiaoxue” <769461729@qq.com>
2021-11-22 13:34:08 +0800

    promise 链式调用实现

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -68,6 +68,7 @@ module.exports = {
           { title: "linux", path: "/sideBar/command/linux" },
           { title: "submodule", path: "/sideBar/command/submodule" },
           { title: "markdown", path: "/sideBar/command/markdown" },
+          { title: "工具地址", path: "/sideBar/command/tool" },
           { title: "git报错", path: "/sideBar/command/gitError" },
         ],
       },
